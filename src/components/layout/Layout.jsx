95335bbbd228039b2e5264e86d328393e1d9a371
Hasan <Hasanaldhahi3@gmail.com>
2022-07-28 00:23:34 +0300

    Have finished without the svg

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,9 @@
 import * as React from "react";
+import Footer from "../footer/Footer";
 
 export default function Layout({ children }) {
     // Put Header or Footer around the children element
-    return <>{children}</>;
+    return <>
+        <Footer></Footer>
+    </>;
 }
