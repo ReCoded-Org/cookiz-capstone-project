a94c1c26e1d86a4faa03bf2f4a3dd19d65dd4ab5
Zisan-uzum <uzummzisann@gmail.com>
2022-07-29 11:15:10 +0300

    feat: add layout

---
+++ src/components/layout/AddMeal.jsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import { HiArrowSmLeft, HiArrowSmRight, HiPlusCircle } from "react-icons/hi";
+import { IoAddCircleOutline, IoTrashBinOutline } from "react-icons/io5";
+export default function AddMeal() {
+    const [dietary, setDietary] = useState([""]);
+
+    function addRemoveDietary(dietaryName) {
+        if (dietary.includes(dietaryName)) {
+            for (let index = 0; index < dietary.length; index++) {
+                if (dietaryName === dietary[index]) {
+                    setDietary([
+                        ...dietary.slice(0, index),
+                        ...dietary.slice(index + 1, dietary.length),
+                    ]);
+                }
+            }
+        } else {
+            setDietary(...dietary, dietaryName);
+        }
+    }
+
+    return (
+        <div className='flex h-full w-full flex-col items-start justify-center gap-3 overflow-hidden'>
+            <div className='mx-0 mt-0 flex h-40 w-full items-center justify-center bg-slate-400 bg-center bg-no-repeat'>
+                <div className='flex items-center justify-between gap-28 md:gap-80 lg:gap-y-96'>
+                    <HiArrowSmLeft />
+                    <HiPlusCircle />
+                    <HiArrowSmRight />
+                </div>
+            </div>
+
+            <input
+                type='text'
+                placeholder='Fried Chips'
+                className='ml-5 w-2/3 rounded border-none bg-[#00494533] text-center text-sm focus:ring-1 focus:ring-[#004945] lg:text-base'
+                required
+            />
+            <p className='ml-3 text-sm lg:text-base'>Dietary</p>
+            <div className='ml-5 flex w-full items-center justify-start gap-3'>
+                <input
+                    type='button'
+                    value='Vegetarian'
+                    className=' click:ring-2  w-1/4 rounded-full bg-[#00494533] p-2 text-center  text-xs text-zinc-800 lg:w-1/3 lg:text-sm'
+                    onClick={(e) => {
+                        addRemoveDietary(e.target.value);
+                    }}
+                />
+
+                <input
+                    type='button'
+                    value='Vegan'
+                    className=' click:ring-2 w-1/4  rounded-full bg-[#00494533] p-2 text-center text-xs  text-zinc-800 lg:w-1/3 lg:text-sm'
+                    onClick={(e) => {
+                        addRemoveDietary(e.target.value);
+                    }}
+                />
+                <input
+                    type='button'
+                    value='Gluten Free'
+                    className=' click:ring-2 w-1/4  rounded-full bg-[#00494533] p-2 text-center text-xs  text-zinc-800 lg:w-1/3 lg:text-sm'
+                    onClick={(e) => {
+                        addRemoveDietary(e.target.value);
+                    }}
+                />
+            </div>
+            <p className='ml-3 text-sm lg:text-base'>Ingredients</p>
+            <div className='ml-5 flex w-full flex-col items-start justify-start gap-3'>
+                <div className='relative w-1/3'>
+                    <input
+                        type='text'
+                        placeholder='Tomatoes'
+                        className=' w-full rounded-full border-none bg-[#00494533] p-2 text-start text-xs focus:ring-1 focus:ring-[#004945] lg:text-sm'
+                    ></input>
+                    <IoAddCircleOutline className='absolute top-2 right-0.5 w-10' />
+                </div>
+
+                <div className='flex w-full items-start justify-start gap-3'>
+                    <div className=' relative w-1/3'>
+                        <p className='w-full rounded-full border-none bg-[#00494533] p-2 text-start text-xs text-zinc-800 lg:text-sm'>
+                            Cucumber
+                        </p>
+                        <IoTrashBinOutline className='absolute top-2 right-0.5 w-10' />
+                    </div>
+                    <div className=' relative w-1/3'>
+                        <p className='w-full rounded-full border-none bg-[#00494533] p-2 text-start text-xs text-zinc-800 lg:text-sm'>
+                            Cucumber
+                        </p>
+                        <IoTrashBinOutline className='absolute top-2 right-0.5 w-10' />
+                    </div>
+                </div>
+            </div>
+            <p className='ml-3 text-sm lg:text-base'>Delivery in</p>
+            <p className='ml-5 text-xs lg:text-base'>30 min</p>
+            <p className='ml-3 text-sm lg:text-base'>Price</p>
+            <p className='ml-5 mb-3 text-xs lg:text-base'>30 dollar</p>
+        </div>
+    );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,27 +1,13 @@
-import Link from "next/link";
-import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import * as React from "react";
 
+import AddMeal from "@/components/layout/AddMeal";
 import Layout from "@/components/layout/Layout";
 
 export default function HomePage() {
-    const { t } = useTranslation("common");
-
     return (
         <Layout>
-            <p>{t("test")}</p>
-            <div style={{ display: "flex", flexDirection: "row", gap: "20px" }}>
-                <Link href='/' locale='en'>
-                    <a>English</a>
-                </Link>
-                <Link href='/' locale='ar'>
-                    <a>Arabic</a>
-                </Link>
-                <Link href='/' locale='tr'>
-                    <a>Turkish</a>
-                </Link>
-            </div>
+            <AddMeal />
         </Layout>
     );
 }
