2e33eacfd158400d91215b0b3753337395a65b54
Zisan-uzum <uzummzisann@gmail.com>
2022-07-30 14:15:15 +0300

    message

--- src/components/layout/AddMeal.jsx
+++ src/components/layout/AddMeal.jsx
@@ -1,35 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { HiArrowSmLeft, HiArrowSmRight, HiPlusCircle } from "react-icons/hi";
 import { IoAddCircleOutline, IoTrashBinOutline } from "react-icons/io5";
 export default function AddMeal() {
     const imageUrl = "../images/header.jpg";
-    const [dietary, setDietary] = useState([""]);
 
-    function addRemoveDietary(dietaryName) {
-        if (dietary.includes(dietaryName)) {
-            for (let index = 0; index < dietary.length; index++) {
-                if (dietaryName === dietary[index]) {
-                    setDietary([
-                        ...dietary.slice(0, index),
-                        ...dietary.slice(index + 1, dietary.length),
-                    ]);
-                }
-            }
-        } else {
-            setDietary(...dietary, dietaryName);
-        }
+    const [meal, setMeal] = useState({
+        imageUrl: "",
+        name: "",
+        dietary: [],
+        ingredients: ["tangerina", "cucumber"],
+        minute: "",
+        price: "",
+    });
+
+    useEffect(() => {
+        console.log(meal);
+    }, [meal]);
+
+    function addIngredient(value) {
+        const obj = {
+            ...meal,
+            ingredients: [...meal["ingredients"], value],
+        };
+        setMeal(obj);
     }
-
     return (
-        <div className='flex h-fit w-[360px] flex-col items-start justify-start gap-3 overflow-hidden bg-red-200'>
+        <div className='flex h-fit w-[340px] flex-col items-start justify-start gap-3 overflow-hidden'>
             <div
                 style={{
                     backgroundImage: `url(${imageUrl})`,
-                    // backgroundSize: "",
-                    width: "100%",
+                    backgroundSize: "cover",
+
                     border: "none",
                 }}
-                className='mx-0 mt-0 flex h-64 w-full items-center justify-center overflow-hidden bg-center bg-no-repeat'
+                className='mx-0 mt-0 flex h-48 w-full items-center justify-center overflow-hidden bg-center bg-no-repeat'
             >
                 <div className='flex items-center justify-between gap-28 text-white md:gap-80 lg:gap-y-96'>
                     <HiArrowSmLeft style={{ fontSize: "40px" }} />
@@ -43,34 +47,53 @@ export default function AddMeal() {
                 placeholder='Fried Chips'
                 className='ml-5 w-2/3 rounded border-none bg-[#00494533] text-center text-sm focus:ring-1 focus:ring-[#004945] lg:text-base'
                 required
+                value={Event.value}
+                onChange={(e) => {
+                    const obj = { ...meal, name: e.target.value };
+                    setMeal(obj);
+                }}
             />
             <p className='ml-3 text-sm lg:text-base'>Dietary</p>
             <div className='ml-5 flex w-full items-center justify-start gap-3'>
-                <input
-                    type='button'
-                    value='Vegetarian'
-                    className=' click:ring-2  w-1/4 rounded-full bg-[#00494533] p-2 text-center  text-xs text-zinc-800 lg:w-1/3 lg:text-sm'
-                    onClick={(e) => {
-                        addRemoveDietary(e.target.value);
-                    }}
-                />
-
-                <input
-                    type='button'
-                    value='Vegan'
-                    className=' click:ring-2 w-1/4  rounded-full bg-[#00494533] p-2 text-center text-xs  text-zinc-800 lg:w-1/3 lg:text-sm'
-                    onClick={(e) => {
-                        addRemoveDietary(e.target.value);
-                    }}
-                />
-                <input
-                    type='button'
-                    value='Gluten Free'
-                    className=' click:ring-2 w-1/4  rounded-full bg-[#00494533] p-2 text-center text-xs  text-zinc-800 lg:w-1/3 lg:text-sm'
-                    onClick={(e) => {
-                        addRemoveDietary(e.target.value);
-                    }}
-                />
+                <label
+                    className=' w-1/4 rounded-full  bg-[#00494533] p-2 text-center text-xs text-zinc-800 lg:w-1/3 lg:text-sm'
+                    htmlFor='vegetarianid'
+                >
+                    <input
+                        type='checkbox'
+                        id='vegetarianid'
+                        value='vegetarianid'
+                        checked=''
+                        className='hidden'
+                    />
+                    Vegetarian
+                </label>
+                <label
+                    className=' w-1/4 rounded-full  bg-[#00494533] p-2 text-center text-xs text-zinc-800 lg:w-1/3 lg:text-sm'
+                    htmlFor='vegetarianid'
+                >
+                    <input
+                        type='checkbox'
+                        id='vegetarianid'
+                        value='vegetarianid'
+                        checked=''
+                        className='hidden'
+                    />
+                    Vegan
+                </label>
+                <label
+                    className=' w-1/4 rounded-full  bg-[#00494533] p-2 text-center text-xs text-zinc-800 lg:w-1/3 lg:text-sm'
+                    htmlFor='vegetarianid'
+                >
+                    <input
+                        type='checkbox'
+                        id='vegetarianid'
+                        value='vegetarianid'
+                        checked=''
+                        className='hidden'
+                    />
+                    Gluten Free
+                </label>
             </div>
             <p className='ml-3 text-sm lg:text-base'>Ingredients</p>
             <div className='ml-5 flex w-full flex-col items-start justify-start gap-3'>
@@ -78,30 +101,49 @@ export default function AddMeal() {
                     <input
                         type='text'
                         placeholder='Tomatoes'
+                        value={Event.value}
                         className=' w-full rounded-full border-none bg-[#00494533] p-2 text-start text-xs focus:ring-1 focus:ring-[#004945] lg:text-sm'
                     ></input>
-                    <IoAddCircleOutline className='absolute top-2 right-0.5 w-10' />
+                    <IoAddCircleOutline
+                        onClick={(e) => {
+                            console.log(e.target.name);
+                            addIngredient(e.target.value);
+                        }}
+                        className='absolute top-2 right-0.5 w-10'
+                    ></IoAddCircleOutline>
                 </div>
 
-                <div className='flex w-full items-start justify-start gap-3'>
-                    <div className=' relative w-1/3'>
-                        <p className='w-full rounded-full border-none bg-[#00494533] p-2 text-start text-xs text-zinc-800 lg:text-sm'>
-                            Cucumber
-                        </p>
-                        <IoTrashBinOutline className='absolute top-2 right-0.5 w-10' />
-                    </div>
-                    <div className=' relative w-1/3'>
-                        <p className='w-full rounded-full border-none bg-[#00494533] p-2 text-start text-xs text-zinc-800 lg:text-sm'>
-                            Cucumber
-                        </p>
-                        <IoTrashBinOutline className='absolute top-2 right-0.5 w-10' />
-                    </div>
+                <div className='flex w-full flex-wrap items-start justify-start gap-3'>
+                    {meal["ingredients"].map((ingredient) => (
+                        // eslint-disable-next-line react/jsx-key
+                        <div className=' relative w-1/3'>
+                            <p className='w-full rounded-full border-none bg-[#00494533] p-2 text-start text-xs text-zinc-800 lg:text-sm'>
+                                {ingredient}
+                            </p>
+                            <IoTrashBinOutline className='absolute top-2 right-0.5 w-10' />
+                        </div>
+                    ))}
                 </div>
             </div>
             <p className='ml-3 text-sm lg:text-base'>Delivery in</p>
-            <p className='ml-5 text-xs lg:text-base'>30 min</p>
+            <input
+                type='text'
+                className='ml-5  rounded border-none text-xs ring-1 ring-[#004945] focus:rounded focus:ring-1 focus:ring-[#004945] lg:text-base'
+                placeholder='30 min'
+                value=''
+            ></input>
+
             <p className='ml-3 text-sm lg:text-base'>Price</p>
-            <p className='ml-5 mb-3 text-xs lg:text-base'>30 dollar</p>
+            <input
+                type='text'
+                className='ml-5 mb-3 rounded border-none text-xs ring-1 ring-[#004945] focus:rounded focus:ring-1 focus:ring-[#004945] lg:text-base'
+                placeholder='30 dollar'
+                value=''
+            ></input>
+
+            <button className=' absolute bottom-72 right-0  rounded bg-[#004945] px-6 py-1 text-white'>
+                Save
+            </button>
         </div>
     );
 }
