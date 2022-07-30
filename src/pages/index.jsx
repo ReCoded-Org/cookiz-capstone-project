3f7157dbf2f7f2e2c06150f5aa16419573e03f66
Zisan-uzum <uzummzisann@gmail.com>
2022-07-30 17:31:39 +0300

    fix: added test

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import * as React from "react";
 
-import AddMeal from "@/components/layout/AddMeal";
+import AddMeal from "@/components/AddMeal/AddMeal";
 import Layout from "@/components/layout/Layout";
 
 export default function HomePage() {
