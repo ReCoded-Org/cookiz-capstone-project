02348ef50596e904e38353cf0a467311ee62ab54
yesim <sityesim@gmail.com>
2022-07-30 12:19:02 +0300

    feat:landing meals added

--- src/components/LandingMeals/LandingMeals.jsx
+++ src/components/LandingMeals/LandingMeals.jsx
@@ -3,12 +3,15 @@ const LandingMeals = () => {
     <div className="grid md:grid-cols-6 m-14 ">
       <div className="md:col-span-1 " >
         <nav>
-          <div className="">
-            <input className="rounded-xl text-base flex justify-center font-bold  
-            " type="search" placeholder="Search" name="search" />
-            <h1 className=  "flex justify-center font-bold pt-5">Categories</h1>
+          <div >
+            <div className="flex justify-center mt-[-14px]">
+              <input className="rounded-xl  font-semibold hover:font-bold" type="search" placeholder="Search" name="search" />
+             </div>
+            <h1 className=  "flex justify-center font-bold pt-8">Categories</h1>
             <ul className="flex items-center flex-col" >
-              <li>item 1</li>
+
+           
+              <li className="font-sans">item 1</li>
               <li>item 2</li>
               <li>item 3</li>
               <li>item 4</li>
@@ -18,7 +21,7 @@ const LandingMeals = () => {
 
         </nav>
         </div>
-        <div className="md:col-span-5 ">
+        <div className="md:col-span-5  ">
         <div className="mt-8 grid md:grid-cols-3 gap-10 m-16" >
 
           <div className=" bg-white rounded overflow-hidden shadow-md relative">
@@ -28,8 +31,8 @@ const LandingMeals = () => {
 
 
             <div>
-              <img className="inline p-2" src="images/Avatar.png" alt="chef" />
-              <span className="text-grey-500 text-sm " >Jennifer Lawrence</span>
+              <img className="inline p-2 md:w-11  w-8" src="images/Avatar.png" alt="chef" />
+              <span className="text-grey-500 md:text-sm font-normal text-xs" >Jennifer Lawrence</span>
             </div>
             <div className=" bg-slate-100  text-xs  uppercase font-bold rounded-full p-2 absolute top-0  ml-2 mt-2 px-3"> <span >60 tl</span></div>
 
@@ -42,8 +45,8 @@ const LandingMeals = () => {
 
 
             <div>
-              <img className="inline p-2 " src="images/Avatar.png" alt="chef" />
-              <span className="text-grey-500 text-sm" >Jennifer Lawrence</span>
+              <img className="inline p-2 md:w-11  w-8 " src="images/Avatar.png" alt="chef" />
+              <span className="text-grey-500 md:text-sm text-xs" >Jennifer Lawrence</span>
             </div>
             <div className=" bg-slate-100  text-xs  uppercase font-bold rounded-full p-2 absolute top-0  ml-2 mt-2 px-3"> <span >60 tl</span></div>
 
@@ -56,8 +59,8 @@ const LandingMeals = () => {
 
 
             <div>
-              <img className="inline p-2 " src="images/Avatar.png" alt="chef" />
-              <span className="text-grey-500 text-sm" >Jennifer Lawrence</span>
+              <img className="inline p-2 md:w-11  w-8" src="images/Avatar.png" alt="chef" />
+              <span className="text-grey-500 md:text-sm text-xs" >Jennifer Lawrence</span>
             </div>
             <div className=" bg-slate-100  text-xs  uppercase font-bold rounded-full p-2 absolute top-0  ml-2 mt-2 px-3"> <span >60 tl</span></div>
 
@@ -70,8 +73,8 @@ const LandingMeals = () => {
 
 
             <div>
-              <img className="inline p-2 " src="images/Avatar.png" alt="chef" />
-              <span className="text-grey-500 text-sm" >Jennifer Lawrence</span>
+              <img className="inline p-2 md:w-11  w-8 " src="images/Avatar.png" alt="chef" />
+              <span className="text-grey-500 md:text-sm text-xs" >Jennifer Lawrence</span>
             </div>
             <div className=" bg-slate-100  text-xs  uppercase font-bold rounded-full p-2 absolute top-0  ml-2 mt-2 px-3"> <span >60 tl</span></div>
 
@@ -84,8 +87,8 @@ const LandingMeals = () => {
 
 
             <div>
-              <img className="inline p-2 " src="images/Avatar.png" alt="chef" />
-              <span className="text-grey-500 text-sm" >Jennifer Lawrence</span>
+              <img className="inline p-2 md:w-11  w-8 " src="images/Avatar.png" alt="chef" />
+              <span className="text-grey-500 md:text-sm text-xs" >Jennifer Lawrence</span>
             </div>
             <div className=" bg-slate-100  text-xs  uppercase font-bold rounded-full p-2 absolute top-0  ml-2 mt-2 px-3"> <span >60 tl</span></div>
 
@@ -98,8 +101,8 @@ const LandingMeals = () => {
 
 
             <div>
-              <img className="inline p-2 " src="images/Avatar.png" alt="chef" />
-              <span className="text-grey-500 text-sm" >Jennifer Lawrence</span>
+              <img className="inline p-2 md:w-11  w-8  " src="images/Avatar.png" alt="chef" />
+              <span className="text-grey-500 md:text-sm text-xs" >Jennifer Lawrence</span>
             </div>
             <div className=" bg-slate-100  text-xs  uppercase font-bold rounded-full p-2 absolute top-0  ml-2 mt-2 px-3"> <span >60 tl</span></div>
 
