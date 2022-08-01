25a2b5cb0bb0170b381d8812846718253c0545b7
yesimsit <88620775+yesimsit@users.noreply.github.com>
2022-08-01 12:04:54 +0300

    Update Cards.jsx

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,43 +1,79 @@
+import Image from "next/image";
 const Cards = () => {
-  return (
-    <div className="flex flex-col justify-center items-center mt-20 ml-10 md:ml-0">
-      {/* title part */}
-      <div className="m-auto bflex flex-col">
-        <div className=" flex  flex-col">
-          <p className=" font-bold text-2xl md:text-3xl text-black">You’ve missed home made food?</p>
-          <p className=" pb-12  pt-2 font-normal   text-black">Pick your favorite chef, food or cousin.</p>
-        </div>
+    return (
+        <div className='mt-20 ml-10 flex flex-col items-center justify-center md:ml-0'>
+            {/* title part */}
+            <div className='bflex m-auto flex-col'>
+                <div className=' flex  flex-col'>
+                    <p className=' text-2xl font-bold text-black md:text-3xl'>
+                        You’ve missed home made food?
+                    </p>
+                    <p className=' pb-12  pt-2 font-normal   text-black'>
+                        Pick your favorite chef, food or cousin.
+                    </p>
+                </div>
 
-
-        <div className="flex flex-row flex-wrap gap-24  w-full justify-between">
-          <div className="flex   flex-row justify-center items-center shadow-lg w-[34.375] h-[17.813rem] rounded-[10px] bg-[#9EE2B8] ">
-            <div className="px-6 flex flex-col ">
-            <img  className=" w-9 md:w-12 pt-4" src="images/cheff.png" />
-              <p className=" w-[307px] text-2xl h-40 font-semibold md:text-3xl text-black">
-                Pick your chef
-                order the food and get it when you want<img  className="w-9 md:w-12  " src="images/soup.png" />
-              </p>
-
+                <div className='flex w-full flex-row flex-wrap  justify-between gap-24'>
+                    <div className='flex   h-[17.813rem] w-[34.375] flex-row items-center justify-center rounded-[10px] bg-[#9EE2B8] shadow-lg '>
+                        <div className='flex flex-col px-6 '>
+                            <div>
+                                <Image
+                                    src='/images/cheff.png'
+                                    width={36}
+                                    height={36}
+                                    layout='intrinsic'
+                                    alt='x'
+                                />
+                            </div>
+                            <p className=' h-40 w-[307px] text-2xl font-semibold text-black md:text-3xl'>
+                                Pick your chef order the food and get it when
+                                you want
+                                <div>
+                                    <Image
+                                        src='/images/soup.png'
+                                        width={36}
+                                        height={36}
+                                        layout='intrinsic'
+                                        alt='x'
+                                    />
+                                </div>
+                            </p>
+                        </div>
+                        <div className='h-[17.8rem] w-full  overflow-hidden rounded-r-lg '>
+                            <Image
+                                src='/images/chef.png'
+                                width={200}
+                                height={284}
+                                layout='intrinsic'
+                                alt='x'
+                            />
+                        </div>
+                    </div>
+                    <div className='flex   h-[17.813rem] w-[34.375] flex-row rounded-[10px] bg-[#FFE7E3] shadow-lg '>
+                        <div className='flex  flex-col justify-center gap-7 px-6 '>
+                            <p className='  w-[310px]   text-2xl   font-semibold text-black md:pt-12 md:text-3xl'>
+                                Get a 15% discount for your first order!
+                            </p>
+                            <button className='  box-border  h-10 w-32 bg-green-900 '>
+                                <span className='h-5 w-20  text-base font-bold leading-5 text-white'>
+                                    Order Now
+                                </span>
+                            </button>
+                        </div>
+                        <div className='h-[17.8rem] w-full  overflow-hidden rounded-r-lg '>
+                            <Image
+                                src='/images/basket.png'
+                                width={200}
+                                height={284}
+                                layout='intrinsic'
+                                alt='x'
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
-            <img className=" w-full  h-[17.8rem]  rounded-r-lg  " src="images/chef.png" alt="" />
-
-          </div>
-          <div className="flex   flex-row shadow-lg w-[34.375] h-[17.813rem] rounded-[10px] bg-[#FFE7E3] ">
-            <div className="px-6  flex flex-col justify-center gap-7 ">
-              <p className="  w-[310px]   text-2xl   md:pt-12 font-semibold md:text-3xl text-black">
-                Get a 15% discount
-                for your first order!
-              </p>
-              <button className="  box-border  w-32 h-10 bg-green-900 "><span className="w-20 h-5  font-bold text-base leading-5 text-white">Order Now</span></button>
-            </div>
-            <img className=" w-full h-[17.8rem]  rounded-r-lg  " src="images/basket.png" alt="" />
-
-          </div>
         </div>
-      </div>
-
-    </div>
-  );
-}
+    );
+};
 
 export default Cards;
