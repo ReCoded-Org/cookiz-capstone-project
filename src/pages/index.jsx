4d51d4e6def0966e13c6e7258453860343e5f6d7
yesimsit <88620775+yesimsit@users.noreply.github.com>
2022-08-01 12:03:21 +0300

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,24 +1,17 @@
-import Link from "next/link";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import * as React from "react";
 
-
-import Layout from "@/components/layout/Layout";
-import New from "@/components/New";
 import Cards from "@/components/Cards/Cards";
+import Layout from "@/components/layout/Layout";
 
 export default function HomePage() {
+    // eslint-disable-next-line
     const { t } = useTranslation("common");
 
     return (
         <Layout>
-           
-
-<Cards/>
-
-
-
+            <Cards />
         </Layout>
     );
 }
