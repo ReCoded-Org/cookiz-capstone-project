e7853b9fdf23c3d3f87a687f323d964634b0c0c2
Ahmet Tezcan <86545756+tzcnahmt@users.noreply.github.com>
2022-08-01 12:42:40 +0300

    Delete src/components directory

--- src/components/Cards/__test__/Cards.test.jsx
+++
@@ -1,7 +0,0 @@
-import renderer from "react-test-renderer";
-import Cards from "../Cards"
-
-it("renders correctly", () => {
-    const tree = renderer.create(<Cards />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
