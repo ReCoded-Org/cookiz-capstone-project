aba2393191798ca39bfb35aeb724e43b61e4b6a5
Hasan <Hasanaldhahi3@gmail.com>
2022-08-02 14:10:01 +0300

    feat: add

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,22 +8,7 @@ import Layout from "@/components/layout/Layout";
 export default function HomePage() {
     const { t } = useTranslation("common");
 
-    return (
-        <Layout>
-            <p>{t("test")}</p>
-            <div style={{ display: "flex", flexDirection: "row", gap: "20px" }}>
-                <Link href='/' locale='en'>
-                    <a>English</a>
-                </Link>
-                <Link href='/' locale='ar'>
-                    <a>Arabic</a>
-                </Link>
-                <Link href='/' locale='tr'>
-                    <a>Turkish</a>
-                </Link>
-            </div>
-        </Layout>
-    );
+    return <Layout></Layout>;
 }
 
 export async function getStaticProps({ locale }) {
