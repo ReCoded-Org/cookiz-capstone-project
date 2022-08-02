724a92c9e9834d1a820c1283b9c04f9bf1c4626c
Zisan-uzum <uzummzisann@gmail.com>
2022-08-02 12:18:39 +0300

    feat: fix conflict

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,19 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import * as React from "react";
 
+import Cards from "@/components/Cards/Cards";
 import Layout from "@/components/layout/Layout";
-import SearchCities from "@/components/SearchCities/SearchCities";
+
 export default function HomePage() {
+    // eslint-disable-next-line
+    const { t } = useTranslation("common");
+
     return (
         <Layout>
-            <SearchCities></SearchCities>
+            <Cards />
         </Layout>
     );
 }
-
 export async function getStaticProps({ locale }) {
     return {
         props: {
