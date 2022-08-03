136036f00780407bb99ae6efffef745f2b2da1c5
Zisan-uzum <uzummzisann@gmail.com>
2022-08-03 23:10:29 +0300

    feat: fix conflict

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,19 @@
+import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import * as React from "react";
 
-import AddMeal from "@/components/AddMeal/AddMeal";
+import Cards from "@/components/Cards/Cards";
+import Hero from "@/components/hero/Hero";
 import Layout from "@/components/layout/Layout";
 
 export default function HomePage() {
+    // eslint-disable-next-line
+    const { t } = useTranslation("common");
+
     return (
         <Layout>
-            <AddMeal />
+            <Hero />
+            <Cards />
         </Layout>
     );
 }
