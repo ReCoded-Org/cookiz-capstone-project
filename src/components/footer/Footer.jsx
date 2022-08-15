37966f75016e048ae712b90158bc619fa51b0811
hiba <hiba.mashfij@gmail.com>
2022-08-15 17:41:32 +0300

    style: runned npm format

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -28,15 +28,15 @@ export default function Footer() {
     return (
         <footer className='bg-black text-white'>
             <div className='grid-row-1 grid h-auto '>
-                <div className='grid grid-cols-1 px-20 py-6 xl:grid-cols-5 '>
+                <div className='xl:grid-cols-5 grid grid-cols-1 px-20 py-6 '>
                     <div className='col-span-2'>
                         <div className='text-3xl font-extrabold hover:text-teal-200'>
                             COOKIEZ
                         </div>
                         <div className='flex flex-row justify-start '>
                             <span
-                                className='mr-4  mt-4 inline-flex  cursor-pointer items-center rounded-full 
-                bg-gray-200 p-2 hover:bg-teal-500  hover:text-gray-100 sm:my-6'
+                                className='sm:my-6  mr-4 mt-4  inline-flex cursor-pointer items-center 
+                rounded-full bg-gray-200 p-2  hover:bg-teal-500 hover:text-gray-100'
                             >
                                 <AiOutlineFacebook
                                     className='text-gray-600'
@@ -44,8 +44,8 @@ export default function Footer() {
                                 />
                             </span>
                             <span
-                                className='mr-4 mt-4 inline-flex cursor-pointer items-center rounded-full bg-gray-200 
-                p-2 hover:bg-teal-500   hover:text-gray-100 sm:my-6'
+                                className='sm:my-6 mr-4 mt-4 inline-flex cursor-pointer items-center rounded-full 
+                bg-gray-200 p-2   hover:bg-teal-500 hover:text-gray-100'
                             >
                                 <AiOutlineTwitter
                                     className='text-gray-600'
@@ -53,8 +53,8 @@ export default function Footer() {
                                 />
                             </span>
                             <span
-                                className='w-50 mr-4 mt-4 inline-flex cursor-pointer items-center rounded-full  bg-gray-200
-                p-2 hover:bg-teal-500   hover:text-gray-100 sm:my-6'
+                                className='w-50 sm:my-6 mr-4 mt-4 inline-flex cursor-pointer items-center  rounded-full
+                bg-gray-200 p-2   hover:bg-teal-500 hover:text-gray-100'
                             >
                                 <AiOutlineInstagram
                                     className='text-gray-600'
