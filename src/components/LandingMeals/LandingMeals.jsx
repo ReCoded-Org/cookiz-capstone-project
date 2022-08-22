6c9dccc5c65876913094049f32ddb1e8e78eef39
yesim <sityesim@gmail.com>
2022-08-22 15:41:58 +0300

    feat: breakpoints in config file deleted

--- src/components/LandingMeals/LandingMeals.jsx
+++ src/components/LandingMeals/LandingMeals.jsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
+import { useTranslation } from "next-i18next";
+
 const LandingMeals = () => {
+    const { t } = useTranslation("landing-meals");
+
     return (
         <div className='md:grid-cols-6 m-14 grid '>
             <div className='md:col-span-1 '>
@@ -9,12 +13,12 @@ const LandingMeals = () => {
                             <input
                                 className='rounded-xl  font-semibold hover:font-bold'
                                 type='search'
-                                placeholder='Search'
+                                placeholder={t("search")}
                                 name='search'
                             />
                         </div>
                         <h1 className='flex justify-center pt-8 font-bold'>
-                            Categories
+                            {t("categories")}
                         </h1>
                         <ul className='flex flex-col items-center'>
                             <li className='font-sans'>item 1</li>
@@ -28,7 +32,7 @@ const LandingMeals = () => {
             </div>
             <div className='md:col-span-5  '>
                 <div className='md:grid-cols-3 m-16 mt-8 grid gap-10'>
-                    <div className='card'>
+                    <div className='card relative'>
                         <Image
                             src='/images/meat.jpg'
                             width='500'
@@ -52,12 +56,12 @@ const LandingMeals = () => {
                                 Jennifer Lawrence
                             </span>
                         </div>
-                        <div className=' badge'>
+                        <div className='absolute top-0 ml-2 mt-2 rounded-full bg-stone-50 px-4 text-sm text-gray-600 text-opacity-80 opacity-100'>
                             {" "}
                             <span>60 tl</span>
                         </div>
                     </div>
-                    <div className='card'>
+                    <div className='card relative'>
                         <Image
                             src='/images/meat.jpg'
                             width='500'
@@ -81,12 +85,12 @@ const LandingMeals = () => {
                                 Jennifer Lawrence
                             </span>
                         </div>
-                        <div className='badge'>
+                        <div className='absolute top-0 ml-2 mt-2 rounded-full bg-stone-50 px-4 text-sm text-gray-600 text-opacity-80 opacity-100'>
                             {" "}
                             <span>60 tl</span>
                         </div>
                     </div>
-                    <div className='card'>
+                    <div className='card relative'>
                         <Image
                             src='/images/meat.jpg'
                             width='500'
@@ -110,12 +114,12 @@ const LandingMeals = () => {
                                 Jennifer Lawrence
                             </span>
                         </div>
-                        <div className='badge'>
+                        <div className='absolute top-0 ml-2 mt-2 rounded-full bg-stone-50 px-4 text-sm text-gray-600 text-opacity-80 opacity-100'>
                             {" "}
                             <span>60 tl</span>
                         </div>
                     </div>
-                    <div className='card'>
+                    <div className='card relative'>
                         <Image
                             src='/images/meat.jpg'
                             width='500'
@@ -139,12 +143,12 @@ const LandingMeals = () => {
                                 Jennifer Lawrence
                             </span>
                         </div>
-                        <div className='badge'>
+                        <div className='absolute top-0 ml-2 mt-2 rounded-full bg-stone-50 px-4 text-sm text-gray-600 text-opacity-80 opacity-100'>
                             {" "}
                             <span>60 tl</span>
                         </div>
                     </div>
-                    <div className='card'>
+                    <div className='card relative'>
                         <Image
                             src='/images/meat.jpg'
                             width='500'
@@ -168,12 +172,12 @@ const LandingMeals = () => {
                                 Jennifer Lawrence
                             </span>
                         </div>
-                        <div className='badge'>
+                        <div className='absolute top-0 ml-2 mt-2 rounded-full bg-stone-50 px-4 text-sm text-gray-600 text-opacity-80 opacity-100'>
                             {" "}
                             <span>60 tl</span>
                         </div>
                     </div>
-                    <div className='card'>
+                    <div className='card relative'>
                         <Image
                             src='/images/meat.jpg'
                             width='500'
@@ -197,7 +201,7 @@ const LandingMeals = () => {
                                 Jennifer Lawrence
                             </span>
                         </div>
-                        <div className='badge'>
+                        <div className='absolute top-0 ml-2 mt-2 rounded-full bg-stone-50 px-4 text-sm text-gray-600 text-opacity-80 opacity-100'>
                             {" "}
                             <span>60 tl</span>
                         </div>
