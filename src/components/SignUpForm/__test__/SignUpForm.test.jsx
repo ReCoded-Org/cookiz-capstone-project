f92ff2e0b512a0dbcd29ad07747fc7d2c55cb238
Marwan Badr <badr0044@algonquinlive.com>
2022-08-22 23:08:13 -0400

    test: tests created for all SignUp components

--- src/components/SignUpForm/__test__/SignUpForm.test.jsx
+++ src/components/SignUpForm/__test__/SignUpForm.test.jsx
@@ -1,10 +1,10 @@
 import renderer from "react-test-renderer";
 
-import SignUpForm from "../SignUpForm";
+import SignUpLanding from "../SignUpForm";
 
 // eslint-disable-next-line
-it("Create Account Form Component", () => {
-    const tree = renderer.create(<SignUpForm />).toJSON();
+it("Create Sign up Landing page Component", () => {
+    const tree = renderer.create(<SignUpLanding />).toJSON();
     // eslint-disable-next-line no-undef
     expect(tree).toMatchSnapshot();
 });
