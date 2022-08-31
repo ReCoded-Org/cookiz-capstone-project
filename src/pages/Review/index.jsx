16810e3bea97103a1cc955d93435b35fc36a6197
nbbhk <hassanshka@gmail.com>
2022-08-31 13:29:43 +0300

    added

---
+++ src/pages/Review/index.jsx
@@ -0,0 +1,9 @@
+import Layout from "@/components/Layout/Layout";
+import Review from "@/components/Review/Review";
+export default function ReviewRoute() {
+    return (
+        <Layout>
+            <Review />
+        </Layout>
+    );
+}
